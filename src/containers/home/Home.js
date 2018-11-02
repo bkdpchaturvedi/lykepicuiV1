7285842997a4b9b5495b7552bbcb12132131629e
wyj5230 <e0000371@u.nus.edu>
2018-11-03 00:12:31 +0800

    introduce dangerouslySetInnerHTML bug and <script> bug

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -16,13 +16,12 @@ export default class Home extends Component {
 
 
   renderLander() {
+     var dirtyUserName = "<img src=x/ onerror=alert(localStorage.access_token)>"
+     var bugStr = "{\"__html\":\"<div className=Home><div className=lander><h1>Lyke Pic App </h1> "+dirtyUserName+"<p>A simple Pic Like App </p></div></div>\"}"
+     var attacker_props = JSON.parse(bugStr)
     return (
-      <div className="Home">
-        <div className="lander">
-          <h1>Lyke Pic App </h1>
-          <p>A simple Pic Like App</p>
-        </div>
-      </div>
+        <div dangerouslySetInnerHTML= {attacker_props}/>
+
     );
   }
  
--- src/containers/others/NotFound.js
+++ src/containers/others/NotFound.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./NotFound.css";
 
-export default () =>
+export default (initialState = {}) =>
   <div className="NotFound">
+      <script>
+          window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
+      </script>
     <h3>Sorry, page not found!</h3>
+{/*dirty  initialState = "{user: { /"username/": "NodeSecurity", /"signature/": "as</script><script>alert('You have an XSS vulnerability!')</script>" } }"*/}
   </div>;

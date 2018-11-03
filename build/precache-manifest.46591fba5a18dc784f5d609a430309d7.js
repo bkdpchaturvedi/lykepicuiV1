73e41b7dd296fbe6e5034da0264e67d64b8b70f5
CN075 <cnyein1@gmail.com>
2018-11-03 16:58:37 +0800

    add compiled file

---
+++ build/precache-manifest.46591fba5a18dc784f5d609a430309d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0108709c02ba45d985f5",
+    "url": "/static/js/main.0108709c.chunk.js"
+  },
+  {
+    "revision": "9b729d11c2eb5ec5d8f7",
+    "url": "/static/js/1.9b729d11.chunk.js"
+  },
+  {
+    "revision": "0108709c02ba45d985f5",
+    "url": "/static/css/main.f4f35ad9.chunk.css"
+  },
+  {
+    "revision": "5983fb86acb40b55e71f46aa3123b777",
+    "url": "/index.html"
+  }
+];
